75ba1be7f9f5ca01bd9df5fcff3b61603774c77e
bk1285 <bk1285@users.noreply.github.com>
2015-01-06 19:07:14 +0100

    Update buttons.py

--- wordclock_tools/buttons.py
+++ wordclock_tools/buttons.py
@@ -1,3 +1,8 @@
+# 3 buttons are required to run the wordclock.
+# Below, for each button, the corresponding GPIO-pin is specified.
+# Note, that it's necessary to set the GPIOs from "high" to "low" 
+# to trigger a signal.
+
 button_left     = 17
 button_return   = 22
 button_right    = 24
