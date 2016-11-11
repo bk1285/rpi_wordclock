9045551cc3e8f69e923d6f4e744b238787ee38b3
Marc Urben <aegnor@mittelerde.ch>
2016-11-11 22:56:58 +0100

    First upload of second swiss german dialect.

---
+++ wordclock_plugins/time_default/time_swiss_german2.py
@@ -0,0 +1,71 @@
+# coding: utf8
+
+import datetime as dt
+
+class time_swiss_german2():
+    '''
+    This class returns a given time as a range of LED-indices.
+    Illuminating these LEDs represents the current time on a swiss_german WCA
+    '''
+
+    def __init__(self):
+        self.prefix = [0] + range(2,5) +  range(6,10)  # -> D ZYT ISCH
+        self.minutes=[[], \
+            # -> FÜF AB
+            range(12,15) + range(36,38), \
+            # -> ZÄÄ AB
+            range(29,32) + range(36,38), \
+            # -> VIERTU AB
+            range(16,21) + range(36,38), \
+            # -> ZWÄNZG AB
+            range(22,28) + range(36,38), \
+            # -> FÜF VOR HAUBI
+            range(12,15) + range(33,36) + range(39,44), \
+            # -> HAUBI
+            range(39,44), \
+            # -> FÜF AB HAUBI
+            range(12,15) + range(36,38) + range(39,44), \
+            # -> ZWÄNZG VOR
+            range(22,28) + range(33,36), \
+            # -> VIERTU VOR
+            range(16,21) + range(33,36), \
+            # -> ZÄÄ VOR
+            range(29,32) + range(33,36), \
+            # -> FÜF VOR
+            range(12,15) + range(33,36) ]
+            # -> ZWÖUFI
+        self.hours= [range(99,105), \
+            # -> EIS
+            range(100,103), \
+            # -> ZWÖI
+            range(44,48), \
+            # -> DRÜ
+            range(84,87), \
+            # -> VIERI
+            range(88,93), \
+            # -> FÜFI
+            range(66,70), \
+            # -> SÄCHSI
+            range(48,54), \
+            # -> SIBNI
+            range(71,76), \
+            # -> ACHTI
+            range(55,60), \
+            # -> NÜNI
+            range(94,98), \
+            # -> ZÄNI
+            range(79,83), \
+            # -> EUFI
+            range(62,66), \
+            # -> ZWÖUFI
+            range(104,110)]
+        self.full_hour= []
+
+    def get_time(self, time, withPrefix=True):
+        hour=time.hour%12+(1 if time.minute/5 > 4 else 0)
+        minute=time.minute/5
+        # Assemble indices
+        return  \
+            (self.prefix if withPrefix else []) + \
+            self.minutes[minute] + \
+            self.hours[hour]
