419d73edbc49aa3c8ada04ee7a66295c44d50994
HeerNMeester <t.perik@gmail.com>
2017-01-11 14:56:26 +0100

    New Dutch 11x10 layout
    New Dutch 11x10 layout, renamed old 10x9 to 'time_dutch_10-9.py'

--- wordclock_plugins/time_default/time_dutch.py
+++ wordclock_plugins/time_default/time_dutch.py
@@ -3,75 +3,75 @@ import datetime as dt
 class time_dutch():
     '''
     This class returns a given time as a range of LED-indices.
-    Illuminating these LEDs represents the current time on a dutch WCA
-    range(0,3) +  range(4,6): HET IS
+    Illuminating these LEDs represents the current time on a dutch WCA 11x10
+    
+	range(0,3) +  range(4,6): HET IS
 
-    range(18,22) + range(51,55): VIJF OVER
-    range(22,26) + range(51,55): TIEN OVER
-    range(33,38) + range(51,55): KWART OVER
-    range(26,33) + range(51,55): TWINTIG OVER
-    range(18,22) + range(40,44) + range(55,59): VIJF VOOR HALF
-    range(55,59): HALF
-    range(18,22) + range(51,55) + range(55,59): VIJF OVER HALF
-    range(26,33) + range(40,44): TWINTIG VOOR
-    range(33,38) + range(40,44): KWART VOOR
-    range(22,26) + range(40,44): TIEN VOOR
-    range(18,22) + range(40,44): VIJF VOOR
+    range(7,11) + range(40,44): VIJF OVER
+    range(11,15) + range(40,44): TIEN OVER
+    range(28,33) + range(40,44): KWART OVER
+    range(11,15) + range(18,22) + range(35,39): TIEN VOOR HALF
+    range(7,11) + range(18,22) + range(35,39): VIJF VOOR HALF
+    range(35,39): HALF
+    range(7,11) + range(22,26) + range(35,39): VIJF OVER HALF
+    range(11,15) + range(22,26) + range(35,39): TIEN OVER HALF
+    range(28,33) + range(44,48): KWART VOOR
+    range(11,15) + range(44,48): TIEN VOOR
+    range(7,11) + range(44,48): VIJF VOOR
 
-    range(104,110): TWAALF
-            range(66,69): EEN
-            range(73,77): TWEE
-            range(77,81): DRIE
-            range(84,88): VIER
-            range(62,66): VIJF
-            range(114,116): ZES
-            range(99,104): ZEVEN
-            range(95,99): ACHT
-            range(88,93): NEGEN
-            range(110,114): TIEN
-            range(81,84): ELF
-            range(104,110): TWAALF
+    range(99,105): TWAALF
+            range(51,54): EEN
+            range(55,59): TWEE
+            range(62,66): DRIE
+            range(66,70): VIER
+            range(70,74): VIJF
+            range(74,77): ZES
+            range(77,82): ZEVEN
+            range(88,92): ACHT
+            range(83,88): NEGEN
+            range(91,95): TIEN
+            range(96,99): ELF
+            range(99,105): TWAALF
        
- self.full_hour= range(118,121)
+ self.full_hour= range(107,110)
     '''
 
     def __init__(self):
         self.prefix = range(0,3) +  range(4,6)
         self.minutes=[[], \
-            range(18,22) + range(51,55), \
-            range(22,26) + range(51,55), \
-            range(33,38) + range(51,55), \
-            range(26,33) + range(51,55), \
-            range(18,22) + range(40,44) + range(55,59), \
+            range(7,11) + range(40,44), \
+            range(11,15) + range(40,44), \
+            range(28,33) + range(40,44), \
+            range(11,15) + range(18,22) + range(35,39), \
+            range(7,11) + range(18,22) + range(35,39), \
+            range(35,39), \
+            range(7,11) + range(22,26) + range(35,39), \
+            range(11,15) + range(22,26) + range(35,39), \
+            range(28,33) + range(44,48), \
+            range(11,15) + range(44,48), \
+            range(7,11) + range(44,48) ]
+        self.hours= [range(99,105), \
+            range(51,54), \
             range(55,59), \
-            range(18,22) + range(51,55) + range(55,59), \
-            range(26,33) + range(40,44), \
-            range(33,38) + range(40,44), \
-            range(22,26) + range(40,44), \
-            range(18,22) + range(40,44) ]
-        self.hours= [range(104,110), \
-            range(66,69), \
-            range(73,77), \
-            range(77,81), \
-            range(84,88), \
             range(62,66), \
-            range(114,116), \
-            range(99,104), \
-            range(95,99), \
-            range(88,93), \
-            range(110,114), \
-            range(81,84), \
-            range(104,110)]
-        self.full_hour= range(118,121)
+            range(66,70), \
+            range(70,74), \
+            range(74,77), \
+            range(77,82), \
+            range(88,92), \
+            range(83,88), \
+            range(91,95), \
+            range(96,99), \
+            range(99,105)]
+        self.full_hour= range(107,110)
 
     def get_time(self, time, withPrefix=True):
-        hour=time.hour%12+(1 if time.minute/5 > 4 else 0)
+        hour=time.hour%12+(1 if time.minute/5 > 3 else 0)
         minute=time.minute/5
         # Assemble indices
         return  \
             (self.prefix if withPrefix else []) + \
             self.minutes[minute] + \
             self.hours[hour] + \
-            ([60] if (hour == 1 and minute != 0) else []) + \
             (self.full_hour if (minute == 0) else [])
 
