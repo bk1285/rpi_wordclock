801eec1c011960a111de50ff07fa7661e035af8f
Bernd Krolla <berndkrolla@web.de>
2017-10-31 19:55:44 +0100

    Adopted leds_off_plugin to latest wordclock version
    Signed-off-by: Bernd Krolla <berndkrolla@web.de>

--- wordclock_plugins/leds_off/plugin.py
+++ wordclock_plugins/leds_off/plugin.py
@@ -1,7 +1,4 @@
 import os
-import time
-import RPi.GPIO as GPIO
-from neopixel import *
 
 class plugin:
     '''
@@ -14,20 +11,13 @@ class plugin:
         '''
         # Get plugin name (according to the folder, it is contained in)
         self.name = os.path.dirname(__file__).split('/')[-1]
+        self.pretty_name = "Leds off"
+        self.description = "Disables the wordclock display."
 
     def run(self, wcd, wci):
         '''
         Displays nothing until aborted by user interaction on pin button_return
         '''
-        emptied = False
-        while True:
-            """Draw nothing."""
-            if not emptied:
-                for i in range(114):
-                    wcd.setPixelColor(i, Color(0, 0, 0))
-                wcd.show()
-                emptied = True
-            if not GPIO.input(22) or GPIO.input(8):
-                print('Pin ' + str(22) + ' pressed.')
-                return
-            time.sleep(20/1000.0)
+        wcd.resetDisplay()
+        wcd.show()
+        wci.waitForEvent()
