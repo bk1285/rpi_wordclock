2a576bc53fc8d7b3178ac7dade9987566a2fadc3
Bernd <bk1285@users.noreply.github.com>
2018-09-12 00:25:05 +0200

    Housekeeping in webinterface
    Fixed indentations
    Added style.css
    
    Signed-off-by: Bernd <bk1285@users.noreply.github.com>

--- wordclock_interfaces/static/js/form.js
+++ wordclock_interfaces/static/js/form.js
@@ -1,33 +1,33 @@
 $(document).ready(function() {
     $.ajax({
-            url: '/api/plugins',
-			type : 'GET'
-            })
-            .done(function(data) {
-                $("#pluginDropdown option").remove();
-                $.each(data.plugins, function(index, item) {
-                    $("#pluginDropdown").append(
-                        $("<option></option>")
-                            .text(item.pretty_name)
-                            .val(item.name)
-                    );
-                });
-            });
+        url: '/api/plugins',
+        type : 'GET'
+    })
+    .done(function(data) {
+        $("#pluginDropdown option").remove();
+        $.each(data.plugins, function(index, item) {
+            $("#pluginDropdown").append(
+                $("<option></option>")
+                    .text(item.pretty_name)
+                    .val(item.name)
+            );
+        });
+    });
     $.ajax({
             url: '/api/plugin',
-			type : 'GET'
-            })
-            .done(function(data) {
-                if (data.error) {
-                    $('#errorAlert').text(data.error).show();
-                    $('#successAlert').hide();
-                    $('#pluginList').text("Error").show();
-                }
-                else {
-                    $('#pluginList').text(data.plugin.description).show();
-                    $("#pluginDropdown").val(data.plugin.name)
-                }
-            });
+            type : 'GET'
+        })
+        .done(function(data) {
+            if (data.error) {
+                $('#errorAlert').text(data.error).show();
+                $('#successAlert').hide();
+                $('#pluginList').text("Error").show();
+            }
+            else {
+                $('#pluginList').text(data.plugin.description).show();
+                $("#pluginDropdown").val(data.plugin.name)
+            }
+        });
 
     $('select').on('change', function() {
 		$.ajax({
@@ -37,7 +37,6 @@ $(document).ready(function() {
 			url : '/api/plugin'
 		})
 		.done(function(data) {
-
 			if (data.error) {
 				$('#errorAlert').text(data.error).show();
 				$('#successAlert').hide();
@@ -51,17 +50,17 @@ $(document).ready(function() {
         $.ajax({
             url: '/api/plugin',
 			type : 'GET'
-			})
-            .done(function(data) {
-                if (data.error) {
-                    $('#errorAlert').text(data.error).show();
-                    $('#successAlert').hide();
-                    $('#pluginList').text("Error").show();
-                }
-                else {
-                    $('#pluginList').text(data.plugin.description).show();
-                    $("#pluginDropdown").val(data.plugin.name)
-                }
-            });
+        })
+        .done(function(data) {
+            if (data.error) {
+                $('#errorAlert').text(data.error).show();
+                $('#successAlert').hide();
+                $('#pluginList').text("Error").show();
+            }
+            else {
+                $('#pluginList').text(data.plugin.description).show();
+                $("#pluginDropdown").val(data.plugin.name)
+            }
+        });
     });
 });
