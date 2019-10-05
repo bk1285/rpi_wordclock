ccdf1b385868b3bab4f359c11cb07b397cb5ae43
Bernd Krolla <berndkrolla@web.de>
2019-10-06 00:57:10 +0200

    Refactoring: Set color temperature via v-dialog

--- wordclock_interfaces/static/js/scripts.js
+++ wordclock_interfaces/static/js/scripts.js
@@ -13,10 +13,9 @@ var vm = new Vue(
 		brightness: undefined,
 		color_temperature: undefined,
         color: {
-            hue: undefined,
-            saturation: undefined,
-            luminosity: undefined,
-            alpha: 1
+			r: undefined,
+			g: undefined,
+			b: undefined
         },
 		apiData: undefined,
 		selectedPlugin: {
@@ -26,7 +25,8 @@ var vm = new Vue(
 		switchWords: true,
 		switchMinutes: true,
 		switchBackground: false,
-		about: false
+		about: false,
+		colorByTemp: false
 	},
 	methods: {
 		loadApi: function () {
@@ -48,14 +48,9 @@ var vm = new Vue(
             this.selectedPlugin = response.data.plugin;
 		},
 		successCallbackColor: function(response) {
-			var h,s,l,r,g,b;
-			r = response.data.words.red;
-			g = response.data.words.green;
-			b = response.data.words.blue;
-			[h,s,l] = rgbToHsl(r, g, b);
-			this.color.hue = h * 360;
-			this.color.saturation = s * 100;
-			this.color.luminosity = l * 100;
+			this.color.r = response.data.words.red;
+			this.color.g = response.data.words.green;
+			this.color.b = response.data.words.blue;
 		},
 		successCallbackBrightness: function(response) {
             this.brightness = response.data;
@@ -108,9 +103,8 @@ var vm = new Vue(
 			xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
 			xmlhttp.send(JSON.stringify({ "color_temperature": color_temperature }));
 		},
-		setColourWheel: function(wheel) {
-			var r,g,b,type;
-			[r,g,b] = hslToRgb(wheel.hue/360, wheel.saturation/100, wheel.luminosity/100);
+		setColourWheel: function(color) {
+			var type;
 			if (this.switchWords) {
 				type = "words"
 			};
@@ -126,18 +120,12 @@ var vm = new Vue(
 			var xmlhttp = new XMLHttpRequest();
 			xmlhttp.open("POST", "/api/color");
 			xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-			xmlhttp.send(JSON.stringify({ "blue": b, "green": g, "red": r , "type": type}));
-		},
-		doSomething: function(color) {
-			console.log(color.rgb.r);
-			console.log(color.rgb.g);
-			console.log(color.rgb.b);
+			xmlhttp.send(JSON.stringify({ "blue": color.rgb.b, "green": color.rgb.g, "red": color.rgb.r , "type": type}));
 		}
 	},
 	beforeMount(){
 		this.loadApi();
 	}
-}
-);
+});
 
-IroColorPicker.on('color:change', vm.doSomething);
+IroColorPicker.on('color:change', vm.setColourWheel);
