a80c610bc709eeb0960677dd77016c17ee094633
Bernd Krolla <berndkrolla@web.de>
2019-10-05 22:58:00 +0200

    Removed old color picker

--- wordclock_interfaces/static/js/scripts.js
+++ wordclock_interfaces/static/js/scripts.js
@@ -1,22 +1,13 @@
-var ColorPicker = window.VueColorPicker;
 var IroColorPicker = window.iro.ColorPicker("#color-picker-container", {
-	// Set the size of the color picker
 	width: 320,
-	// Set the initial color to pure red
 	color: "#fcc"
   });
-
-function onColorChange(color, changes) {
-	// print the color's new hex value to the developer console
-	this.$root.$emit('clickedSomething')
-	console.log(color.hexString);
-}
 
 var vm = new Vue(
 {
 	el: '#app',
 	components: {
-                ColorPicker: ColorPicker
+		IroColorPicker: IroColorPicker
 	},
 	data: {
 		brightness: undefined,
@@ -138,8 +129,9 @@ var vm = new Vue(
 			xmlhttp.send(JSON.stringify({ "blue": b, "green": g, "red": r , "type": type}));
 		},
 		doSomething: function(color) {
-			console.log("we did it!");
-			console.log(color.rgb.r)
+			console.log(color.rgb.r);
+			console.log(color.rgb.g);
+			console.log(color.rgb.b);
 		}
 	},
 	beforeMount(){
@@ -149,72 +141,3 @@ var vm = new Vue(
 );
 
 IroColorPicker.on('color:change', vm.doSomething);
-
-
-/**
- * Converts an HSL color value to RGB. Conversion formula
- * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
- * Assumes h, s, and l are contained in the set [0, 1] and
- * returns r, g, and b in the set [0, 255].
- *
- * @param   {number}  h       The hue
- * @param   {number}  s       The saturation
- * @param   {number}  l       The lightness
- * @return  {Array}           The RGB representation
- */
-function hslToRgb(h, s, l){
-    var r, g, b;
-
-    if(s == 0){
-        r = g = b = l; // achromatic
-    }else{
-        var hue2rgb = function hue2rgb(p, q, t){
-            if(t < 0) t += 1;
-            if(t > 1) t -= 1;
-            if(t < 1/6) return p + (q - p) * 6 * t;
-            if(t < 1/2) return q;
-            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
-            return p;
-        }
-
-        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-        var p = 2 * l - q;
-        r = hue2rgb(p, q, h + 1/3);
-        g = hue2rgb(p, q, h);
-        b = hue2rgb(p, q, h - 1/3);
-    }
-
-    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
-}
-
-/**
- * Converts an RGB color value to HSL. Conversion formula
- * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
- * Assumes r, g, and b are contained in the set [0, 255] and
- * returns h, s, and l in the set [0, 1].
- *
- * @param   {number}  r       The red color value
- * @param   {number}  g       The green color value
- * @param   {number}  b       The blue color value
- * @return  {Array}           The HSL representation
- */
-function rgbToHsl(r, g, b){
-    r /= 255, g /= 255, b /= 255;
-    var max = Math.max(r, g, b), min = Math.min(r, g, b);
-    var h, s, l = (max + min) / 2;
-
-    if(max == min){
-        h = s = 0; // achromatic
-    }else{
-        var d = max - min;
-        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-        switch(max){
-            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-            case g: h = (b - r) / d + 2; break;
-            case b: h = (r - g) / d + 4; break;
-        }
-        h /= 6;
-    }
-
-    return [h, s, l];
-}
