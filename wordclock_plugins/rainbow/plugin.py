a9adb3fc864beab28894f49c294e9dbafd2c1e29
plotaBot <>
2020-05-04 14:29:23 +0200

    removed import neopixel to wordclock_colors

--- wordclock_plugins/rainbow/plugin.py
+++ wordclock_plugins/rainbow/plugin.py
@@ -1,5 +1,5 @@
 import os
-from neopixel import *
+import wordclock_tools.wordclock_colors as wcc
 
 
 class plugin:
@@ -37,10 +37,10 @@ class plugin:
         Generate rainbow colors across 0-255 positions.
         """
         if pos < 85:
-            return Color(pos * 3, 255 - pos * 3, 0)
+            return wcc.Color(pos * 3, 255 - pos * 3, 0)
         elif pos < 170:
             pos -= 85
-            return Color(255 - pos * 3, 0, pos * 3)
+            return wcc.Color(255 - pos * 3, 0, pos * 3)
         else:
             pos -= 170
-            return Color(0, pos * 3, 255 - pos * 3)
+            return wcc.Color(0, pos * 3, 255 - pos * 3)
