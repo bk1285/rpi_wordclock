a9b49b640135b9713db8a4417193b54e08080672
Pascal Henze <ph@i-novation.de>
2021-12-15 07:23:56 +0100

    fixed time_swedish

--- wordclock_plugins/time_default/time_swedish.py
+++ wordclock_plugins/time_default/time_swedish.py
@@ -2,36 +2,36 @@
 class time_swedish:
     """
     This class returns a given time as a range of LED-indices.
-    Illuminating these LEDs represents the current time on a german WCA
+    Illuminating these LEDs represents the current time on a swedish WCA
     """
 
     def __init__(self):
         self.prefix = list(range(0,7)) +  list(range(8,10))
         self.minutes=[[], \
-            list(range(12,15)) + list(range(35,39)), \
-            list(range(11,15)) + list(range(35,39)), \
-            list(range(26,33)) + list(range(35,39)), \
-            list(range(15,22)) + list(range(35,39)), \
-            list(range(12,15)) + list(range(39,42)) + list(range(44,48)), \
-            list(range(44,48)), \
-            list(range(7,11)) + list(range(35,39)) + list(range(44,48)), \
-            list(range(15,22)) + list(range(39,42)), \
-            list(range(26,33)) + list(range(39,42)), \
-            list(range(11,15)) + list(range(39,42)), \
-            list(range(12,15)) + list(range(39,42)) ]
-        self.hours= [list(range(49,54)), \
-            list(range(57,60)), \
-            list(range(55,59)), \
-            list(range(67,71)), \
-            list(range(84,88)), \
+            list(range(11,14)) + list(range(44,48)), \
+            list(range(99,102)) + list(range(44,48)), \
+            list(range(22,27)) + list(range(44,48)), \
+            list(range(33,38)) + list(range(44,48)), \
+            list(range(11,14)) + list(range(15,16)) + list(range(51,55)), \
+            list(range(51,55)), \
+            list(range(33,38)) + list(range(44,48)) + list(range(51,55)), \
+            list(range(33,38)) + list(range(39,41)), \
+            list(range(22,27)) + list(range(39,41)), \
+            list(range(17,20)) + list(range(39,41)), \
+            list(range(11,14)) + list(range(39,41)) ]
+        self.hours= [list(range(106,110)), \
+            list(range(55,58)), \
+            list(range(63,66)), \
+            list(range(66,69)), \
             list(range(73,77)), \
-            list(range(100,105)), \
-            list(range(60,66)), \
-            list(range(89,93)), \
-            list(range(80,84)), \
-            list(range(93,97)), \
             list(range(77,80)), \
-            list(range(49,54))]
+            list(range(85,88)), \
+            list(range(88,91)), \
+            list(range(91,95)), \
+            list(range(96,99)), \
+            list(range(99,102)), \
+            list(range(102,106)), \
+            list(range(106,110))]
         self.full_hour= list(range(107,110))
 
     def get_time(self, time, purist):
@@ -40,7 +40,7 @@ class time_swedish:
         # Assemble indices
         return  \
             (self.prefix if not purist else []) + \
-            self.minutes[int(minute)] + \
+            self.minutes[minute] + \
             self.hours[hour] + \
             ([60] if (hour == 1 and minute != 0) else []) + \
             (self.full_hour if (minute == 0) else [])
